2a8abd6020583c0ec82c082b1b6bf70564790cb2
mgallaoui33-oss <mgallaoui33@gmail.com>
2026-05-02 10:20:33 -1100

    Update index.php

--- index.php
+++ index.php
@@ -55,6 +55,9 @@ if (isset($_POST['register'])) {
 <head>
     <meta charset="UTF-8">
     <title>تسجيل الفلاحين | مشروع الثورة</title>
+    <link rel="manifest" href="manifest.json">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="theme-color" content="#d32f2f">
     <style>
         :root { --primary: #2ecc71; --dark: #1a1a1a; }
         body { background: var(--dark); color: white; font-family: 'Arial', sans-serif; display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; }
